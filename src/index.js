05fabf4a0f42978a5be78f057ed5957056967181
mirageN1349 <desertirpipi@gmail.com>
2020-07-06 06:07:57 +0300

    validate all

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const inputLink = document.querySelector('.input-link')
 const inputTime = document.querySelector('.minute')
 const btnStop = document.querySelector('.button-stop')
 let arrLink = []
+let validInput = /^[0-9]+$/
 
 form.addEventListener('submit', e => {
   e.preventDefault()
@@ -12,9 +13,29 @@ form.addEventListener('submit', e => {
     confirm(`введите ссылки или время!`)
     return
   }
-  btnStop.classList.add('active')
+
+  if (!validInput.test(inputTime.value)) {
+    alert(`${inputTime.value} не является временем`)
+    return
+  }
+
   arrLink = inputLink.value.split(',')
+
+  let flag = true
+  arrLink.forEach(link => {
+    let check = /((http|https):\/\/(www\.)?[a-zа-я0-9-]+\.[a-zа-я0-9-]{2,6})/i.exec(
+      link
+    )
+    if (!check) {
+      alert(`${link} не является ссылкой!`)
+      flag = false
+    }
+  })
+
+  if (!flag) return
+
   const timeToRestart = inputTime.value
+  btnStop.classList.add('active')
 
   drops(timeToRestart, arrLink)
 })
